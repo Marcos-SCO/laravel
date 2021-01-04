089640f06cb6d0c1e562426f7ac0aebd2c8c9632
Marcos-SCO <marcos_sco@outlook.com>
2021-01-04 18:45:25 -0300

    Create likes implementation

--- blogs/crash_course_project/resources/views/posts/index.blade.php
+++ blogs/crash_course_project/resources/views/posts/index.blade.php
@@ -31,6 +31,17 @@
 
             <p class="mb-2">{{ $post->body }}</p>
 
+            <div class="flex items-center">
+                <form action="" method="post" class="mr-1">
+                    @csrf
+                    <button type="submit" class="text-blue-500">Like</button>
+                </form>
+                <form action="" method="post" class="mr-1">
+                    @csrf
+                    <button type="submit" class="text-blue-500">Unlike</button>
+                </form>
+            </div>
+
         </div>
         @endforeach
 
