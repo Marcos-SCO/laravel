f759af1d45ccae62b5e56298d441413985513722
Marcos-SCO <marcos_sco@outlook.com>
2021-09-22 22:33:17 -0300

    authors

--- ePetitions/app/Http/Controllers/AuthorController.php
+++ ePetitions/app/Http/Controllers/AuthorController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\AuthorCollection;
+use App\Http\Resources\AuthorResource;
 use App\Models\Author;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AuthorController extends Controller
 {
@@ -14,7 +17,9 @@ class AuthorController extends Controller
      */
     public function index()
     {
-        //
+        $authors = Author::all();
+
+        return new AuthorCollection($authors);
     }
 
     /**
@@ -25,7 +30,6 @@ class AuthorController extends Controller
      */
     public function show(Author $author)
     {
-        //
+        return new AuthorResource($author, Response::HTTP_OK);
     }
-
 }
