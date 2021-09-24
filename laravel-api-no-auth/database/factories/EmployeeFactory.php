fa98dd906fb4dec40e5d5a4452d4dc163c0005ac
Marcos-SCO <marcos_sco@outlook.com>
2021-09-24 16:39:28 -0300

    employee initial conifg

---
+++ laravel-api-no-auth/database/factories/EmployeeFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Employee;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EmployeeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Employee::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->safeEmail,
+            'phone_number' => $this->faker->phoneNumber,
+            'age' => $this->faker->numberBetween(20, 45),
+            'gender' => $this->faker->randomElement([
+                'male',
+                'female',
+            ]),
+        ];
+    }
+}
