079145d5449537eb9b6f30981096cd48129bd3c5
Marcos-SCO <marcos_sco@outlook.com>
2021-09-25 21:46:08 -0300

    fillable models

--- laravel-api-sanctum/app/Models/Project.php
+++ laravel-api-sanctum/app/Models/Project.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+    
+    protected $fillable = ['name', 'student_id', 'description', 'phone_number'];
 }
