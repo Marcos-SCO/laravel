87aaf663bf09fef58879e0d04bc179555f920823
Marcos-SCO <marcos_sco@outlook.com>
2021-09-27 13:12:38 -0300

    initial commit

---
+++ laravel-jwt-api/database/migrations/2021_09_25_230953_project.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Project extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            // $table->integer('student_id')->unsigned();
+            $table->foreignId('student_id')->references('id')->on('students')->onDelete('cascade');
+            $table->string('name', 150);
+            $table->text('description');
+            $table->integer('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+
+    }
+}
