8043745ccce96b1ccbca3c3d65c41b827e8f77e9
Marcos-SCO <marcos_sco@outlook.com>
2021-09-29 14:19:46 -0300

    sass in app js

--- eCommerce/resources/views/login.blade.php
+++ eCommerce/resources/views/login.blade.php
@@ -4,6 +4,22 @@
 
 <h1>Login Page</h1>
 
-<button class="btn btn-primary">Login</button>
+<div class="container my-5">
+  <div class="row align-item-center">
+    <div class="form-container">
+      <form action="">
+        <div class="div form-group">
+          <label for="email"></label>
+          <input type="email" name="email" id="email" class="form-control">
+        </div>
+        <div class="div form-group">
+          <label for="password"></label>
+          <input type="password" name="password" id="password" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+    </div>
+  </div>
+</div>
 
 @endSection
