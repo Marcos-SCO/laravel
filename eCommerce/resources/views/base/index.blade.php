a51ade49b244ffa6cfc5ff253a0809b3cc9a06ec
Marcos-SCO <marcos_sco@outlook.com>
2021-09-29 14:06:14 -0300

    header and footer

--- eCommerce/resources/views/base/index.blade.php
+++ eCommerce/resources/views/base/index.blade.php
@@ -12,12 +12,14 @@
 </head>
 
 <body>
+  {{View::make('base/header')}}
 
   @yield('content')
 
+  {{View::make('base/footer')}}
+
   <!-- Bootstrap js cdn -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
   <!-- App -->
   <script src="{{asset('js/app.js')}}"></script>
 </body>
