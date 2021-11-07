951c385cca41ba0881227832fb35f87a31e5eaf4
Marcos-SCO <marcos_sco@outlook.com>
2021-11-06 22:00:59 -0300

    employees

--- employees/routes/api.php
+++ employees/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\EmployeeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('employees', [EmployeeController::class])->names([
+    'index' => 'api.employee.index',
+    'create' => 'api.employee.create',
+    'store' => 'api.employee.store',
+    'edit' => 'api.employee.edit',
+    'update' => 'api.employee.update',
+    'destroy' => 'api.employee.delete',
+]);
